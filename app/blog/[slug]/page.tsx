240d0659a27c1f59d7825a9955cd5aa446f76005
imran752310 <muhammadimran752310@gmail.com>
2025-04-27 17:13:01 +0500

    ok

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -17,7 +17,7 @@ export default  function Page() {
         width={400}
         height={400}
       />
-      <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-12 uppercase pt-20">
+      <h1 className="text-3xl md:text-4xl font-bold text-gray-800 dark:text-white mb-12 uppercase pt-20">
       Mastering React for Modern Web Development
       </h1>
       <p>
